c6c6dcb6a9ce09ba487617b5d426605c3f8ebec8
carlos e rafael <carlosrafaelgit@gmail.com>
2023-09-06 09:44:47 -0300

    entrega da ativid: incompleta

--- trabai.py
+++ trabai.py
@@ -1,10 +1,38 @@
+'''
+EXERCÍCIO: [UTILIZAR OS RECURSOS ENSINADOS EM AULA]
+
+PARTE I:
+Um usuário necessita se cadastrar para entrar em um sistema.
+Para tal ele deve digitar um e-mail e uma senha.
+
+Pedir a digitação do e-mail.
+Requisito:
+- a string ter ao menos um arroba e um ponto 
+	- ao menos uma letra antes do arroba
+	- ao menos uma letra entre o arroba e ponto.
+	- ao menos uma letra depois do ponto
+	- no máximo dois pontos depois do arroba
+
+PARTE II:
+Pedir a digitação da senha.
+Requisitos:
+- Ao menos 6 caracteres
+- Ao menos uma letra maiúscula
+- Ao menos um número
+- Ao menos um caractere especial
+
+Depois da digitação, caso não atenda aos requisitos, exibir uma mensagem dizendo o que está faltando.
+
+Caso a senha seja validada, pedir a redigitação da senha para conferir.
+[neste momento não se preocupe em aparecer a senha e não o *]
+'''
+
 ema = input("Digite seu email: ")
 
 def cadastro(ema) -> str:
-    cont = 0
     while True: 
         if '@' in ema and '.' in ema:
-            if ema[cont + 1:] != '@':
+            if ema[0:] != '@':
                 ind = ema.index('@')
                 if ema[ind + 1:] != '.':
                     ind2 = ema.index('.')
@@ -13,10 +41,17 @@ def cadastro(ema) -> str:
                             return ema 
                         elif ema.count('.') <= 0 and ema.count('.') > 2:
                             print("Passou de dois pontos ou não tem")
-                    else:         
+                    elif ema[-1] != '.':
+                        
+                else:
+                    print("não tem letras entre o arroba e ponto")
+            else:
+                print("Não tem letras antes do arroba")
+        elif not '@' in ema or not '.' in ema:
+            print("Esta faltando arroba e ponto no email")         
 
-x = cadastro(ema)
-print(x)
+result = cadastro(ema)
+print(result)
 
 '''
 if len(sen) < 6:
